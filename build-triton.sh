78265f2f431cd78ca767cd10a036bde9d42d611b
osdnk <micosa97@gmail.com>
2025-08-10 22:00:58 +0300

    tr

--- build-triton.sh
+++ build-triton.sh
@@ -8,9 +8,7 @@ make hexl-triton
 make wrapper
 export LD_LIBRARY_PATH=./hexl-bindings/hexl/build/hexl/lib:$(pwd)
 export RUSTFLAGS="-C linker=gcc"
-cargo run
-cargo test
-cargo bench
+RUSTFLAGS="-C target-feature=+avx2,+avx,+sse2,+avx512f,+avx512bw" cargo bench
 
 
 
